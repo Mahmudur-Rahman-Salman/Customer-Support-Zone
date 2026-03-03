21ab4f77ec19f09d16c755913609960d8ce66ef2
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2026-03-04 04:29:00 +0600

    banner section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
     <>
-     <div className='bg-white'>
-      <Navbar></Navbar>
-     </div>
+      <div className="bg-white">
+        <Navbar></Navbar>
+        <Banner></Banner>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,25 @@
+const Banner = ({ inProgress = 0, resolved = 0 }) => {
+  return (
+    <>
+      <section className="max-w-7xl mx-auto px-6 mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {/* In Progress */}
+          <div className="banner-card banner-progress">
+            <p className="text-white/90 text-lg font-medium">In-Progress</p>
+            <p className="text-white text-5xl font-semibold mt-4">
+              {inProgress}
+            </p>
+          </div>
+
+          {/* Resolved */}
+          <div className="banner-card banner-resolved">
+            <p className="text-white/90 text-lg font-medium">Resolved</p>
+            <p className="text-white text-5xl font-semibold mt-4">{resolved}</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Banner;
